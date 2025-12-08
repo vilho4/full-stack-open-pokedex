3e10a9632b721eaf5e8b050f7ae7d9e1976ebefd
vilho4 <ville.j.holopainen@student.jyu.fi>
2025-12-08 19:24:41 +0200

    health check endpoint

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// health endpoint
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 // version endpoint
 app.get('/version', (req, res) => {
   res.send('3, render hook uusiksi')
