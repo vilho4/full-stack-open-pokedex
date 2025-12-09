ef3520f1fcda360535325dab408751c0827df533
vilho4 <ville.j.holopainen@student.jyu.fi>
2025-12-09 19:23:11 +0200

    fixed health check endpoint

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-// // health endpoint
+  // health endpoint
+ app.get('/health', (req, res) => {
+   res.send('ok')
+ })
+
 // app.get('/health', (req, res) => {
+//   if (true) throw('error...  ')
 //   res.send('ok')
 // })
 
-app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
-  res.send('ok')
-})
-
 // version endpoint
 app.get('/version', (req, res) => {
-  res.send('3, render hook uusiksi')
+  res.send('4')
 })
 
 app.listen(PORT, () => {
